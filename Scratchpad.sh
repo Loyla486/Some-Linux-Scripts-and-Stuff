c5869c725e5263401abd1406e81d6c3e90465345
Loyla - i486 <159725294+Loyla486@users.noreply.github.com>
2026-09-15 17:46:13 -0700

    Update Scratchpad.sh

--- Scratchpad.sh
+++ Scratchpad.sh
@@ -1,1 +1,9 @@
-sudo apt install mpv smplayer  
+sudo apt install mpv smplayer
+
+# cdrdao:
+# Debian/Ubuntu
+sudo apt install cdrdao 
+sudo apt install cdda2wav
+
+# Arch
+sudo pacman -S cdrtools   
